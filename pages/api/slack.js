736fc6b328b43a6aa63c2dda65ffec4f61a53ee3
usha-takeshige <kiyo97007@gmail.com>
2025-07-14 21:04:26 +0900

    fix: CORSの対応

--- pages/api/slack.js
+++ pages/api/slack.js
@@ -3,6 +3,9 @@ import { WebClient } from '@slack/web-api';
 export default async function handler(req, res) {
   // OPTIONSリクエストの処理
   if (req.method === 'OPTIONS') {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     res.status(200).end();
     return;
   }
