b120974e19ce7e5b271e1f87a7a0f2b1fe557e39
usha-takeshige <kiyo97007@gmail.com>
2025-07-14 21:24:39 +0900

    fix: デバック用コード

--- api/slack.js
+++ api/slack.js
@@ -1,16 +1,34 @@
 export default async function handler(req, res) {
-  console.log('Method:', req.method);
-  console.log('Body:', req.body);
-  console.log('Query:', req.query);
-  
-  // チャレンジレスポンス
-  if (req.body && req.body.challenge) {
-    return res.status(200).send(req.body.challenge);
+  try {
+    console.log('=== SLACK WEBHOOK DEBUG ===');
+    console.log('Method:', req.method);
+    console.log('Content-Type:', req.headers['content-type']);
+    console.log('Body type:', typeof req.body);
+    console.log('Body:', req.body);
+    
+    // bodyが文字列の場合はパース
+    let data = req.body;
+    if (typeof req.body === 'string') {
+      try {
+        data = JSON.parse(req.body);
+      } catch (e) {
+        console.log('JSON parse error:', e);
+      }
+    }
+    
+    console.log('Parsed data:', data);
+    
+    // チャレンジレスポンス
+    if (data && data.challenge) {
+      console.log('Sending challenge response:', data.challenge);
+      return res.status(200).send(data.challenge);
+    }
+    
+    console.log('No challenge, sending OK');
+    return res.status(200).send('OK');
+    
+  } catch (error) {
+    console.error('Error:', error);
+    return res.status(500).send('Error');
   }
-  
-  if (req.query && req.query.challenge) {
-    return res.status(200).send(req.query.challenge);
-  }
-  
-  return res.status(200).json({ ok: true });
 }
